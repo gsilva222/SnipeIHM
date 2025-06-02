570b5c7e893526f772fd200c84337d559c4eec32
gsilva222 <goncaloamesilva@gmail.com>
2025-06-02 16:05:55 +0100

    so adicionei imports

--- Snipe/src/app/pages/logo/logo.page.ts
+++ Snipe/src/app/pages/logo/logo.page.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { IonContent, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-logo',
   templateUrl: 'logo.page.html',
   styleUrls: ['logo.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonButton],
 })
 export class LogoPage {
   constructor() {}
--- Snipe/src/app/pages/register/register.page.ts
+++ Snipe/src/app/pages/register/register.page.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+  ],
 })
 export class RegisterPage {
   constructor() {}
