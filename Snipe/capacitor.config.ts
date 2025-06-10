2e58160d5dd7187f03bba4fa47bf337968df9828
gsilva222 <goncaloamesilva@gmail.com>
2025-06-10 21:26:22 +0100

    Quase quase

--- Snipe/capacitor.config.ts
+++ Snipe/capacitor.config.ts
@@ -26,8 +26,7 @@ const config: CapacitorConfig = {
       smallIcon: 'ic_stat_icon_config_sample',
       iconColor: '#488AFF',
       sound: 'beep.wav',
-    },
-    PushNotifications: {
+    },    PushNotifications: {
       presentationOptions: ['badge', 'sound', 'alert'],
     },
   },
